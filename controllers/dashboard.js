75bc1d41990fdb50fc310a6cc65b13cf897d0681
André Alves <andre20mesquita@gmail.com>
2022-04-22 17:53:41 -0300

    adicionando dashboard do funcionario

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,7 @@
 const isAuth = require("../middlewares/auth");
 const moment = require("moment"); // require
 const Extra = require("../models/extra");
+const Instalacoes = require("../models/instalacoes");
 
 module.exports = (app) => {
   app.get("/dashboard", isAuth, async (req, res) => {
@@ -8,19 +9,77 @@ module.exports = (app) => {
   });
 
   app.get("/dashboard-funcionario", isAuth, async (req, res) => {
+    moment.locale("pt");
+    moment.updateLocale("pt", {
+      months: [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+      ],
+    });
+
+    let meses = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ];
+
+    let mes = moment().format("MMMM");
+    let ano = moment().format("YYYY");
+
     let funcionarios = await Extra.buscarTodosFuncionarios(); //buscar todos funcionarios sgp
 
     res.render("dashboard-funcionario/index", {
       funcionarios: funcionarios.rows,
+      mes: mes,
+      ano: ano,
+      meses: meses,
     });
   });
 
   app.post("/dashboard-funcionario", isAuth, async (req, res) => {
-    let funcionarios = await Extra.buscarTodosFuncionarios(); //buscar todos funcionarios sgp
+    let funcionario = req.body.funcionario;
+    let ano = req.body.ano;
+    let mes = req.body.mes;
 
-    res.render("dashboard-funcionario/index", {
-      funcionarios: funcionarios.rows,
-    });
+    let dataInicioMes = moment([ano, mes])
+      .startOf("month")
+      .format("YYYY-MM-DD");
+
+    let dataFimMes = moment([ano, mes]).endOf("month").format("YYYY-MM-DD");
+
+    let queryDataInicio = ` and TO_DATE(to_char(ao2.data_finalizacao,'YYYY-MM-DD'),'YYYY-MM-DD') >= TO_DATE(to_char(date('${dataInicioMes}'),'YYYY-MM-DD'),'YYYY-MM-DD') `;
+
+    let queryDataFim = ` and TO_DATE(to_char(ao2.data_finalizacao,'YYYY-MM-DD'),'YYYY-MM-DD') <= TO_DATE(to_char(date('${dataFimMes}') ,'YYYY-MM-DD'),'YYYY-MM-DD') `;
+
+    let queryFuncionario = ` and av.nome = '${funcionario}' `;
+    let queryData = ` ${queryDataInicio} ${queryDataFim} `;
+
+    let queryFinal = ` ${queryFuncionario} ${queryData} `;
+
+    console.log(queryFinal);
+
+    let instalacoes = await Instalacoes.pegarInstalacoes(queryFinal);
+
+    res.status(200).json(instalacoes.rows);
   });
 
   app.get("/dashboard-valor-mes", isAuth, async (req, res) => {
